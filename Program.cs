e9e7e436f65c270e45eca382cc5258d5f48510fc
Ramon Mesquita <ramonmesquitamacedo2222@gmail.com>
2023-04-29 14:04:07 -0300

    Exercício concluído

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Cryptography;
+
+Console.WriteLine("--- Rolagem de Dado ---\n");
+
+Console.Write("Quantas faces? ");
+int faces = Convert.ToInt32(Console.Read());
+
+int rolagem = RandomNumberGenerator.GetInt32(1, faces + 1);
+
+Console.WriteLine($"\nRolagem: {rolagem}");
